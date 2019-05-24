7d08ee1b89f23fb71bca67cbad8c4a0ec345a6df
Raphael <raphael.dalmon@epitech.eu>
2019-05-24 11:11:06 +0200

    Propagation by layer forward

---
+++ src/layer/propagate.c
@@ -0,0 +1,45 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2019 Raphael DALMON
+
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+
+#include "layer.h"
+
+
+/*
+    Layer layer got values from minus
+
+    o               o
+    o               o
+    o               o
+                    o
+
+    x1 = [1, 3]     x2 = [1, 4]
+    theta1 = [3, 4]
+*/
+
+void layer_forward(layer_t *minus, layer_t *layer)
+{
+    matrix_t *res;
+
+    if (!minus || !layer)
+        return;
+    res = matrix_mul(minus->values, minus->theta);
+}
